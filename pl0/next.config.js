3eaa64955e08a61fdad5531b925dffe4038767f2
Richard Lipka <lipka@kiv.zcu.cz>
2026-09-10 08:40:48 +0200

    build: support BASE_PATH environment variable for subpath export deployment

--- pl0/next.config.js
+++ pl0/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.BASE_PATH || '';
+
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
+    basePath: basePath || undefined,
+    assetPrefix: basePath ? `${basePath}/` : undefined,
     images: {
         unoptimized: true,
     },
 };
 
 module.exports = nextConfig;
-
